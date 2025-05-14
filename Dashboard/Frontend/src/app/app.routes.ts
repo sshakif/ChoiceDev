160152ae06890bb6ae7f0c3d0728e90a3894c90c
arifht25 <harif@hybri.tech>
2025-05-14 13:19:41 +0600

    issue stayle.scss

--- Dashboard/Frontend/src/app/app.routes.ts
+++ Dashboard/Frontend/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ServiceListComponent } from '@app/service-page/service-list/service-list.component';
 
 export const routes: Routes = [
 
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'service-list',
+    component: ServiceListComponent,
   },
   
 ];
