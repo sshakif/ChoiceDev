0591c7bd84adfb384f3886441f9713d497c4bc33
arifht25 <harif@hybri.tech>
2025-05-14 10:29:01 +0600

    delete servicepagecontroller and api

--- Dashboard/Backend/routes/api.php
+++ Dashboard/Backend/routes/api.php
@@ -2,12 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ServicePageController;
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-// Service routes
-Route::resource('services', ServicePageController::class);
+
